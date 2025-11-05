d9e02b4644b230ed7facbc33d6881640ffc651a5
IndieMasco <s.clarkyy.1994@gmail.com>
2025-11-05 20:05:49 +0000

    changed the title for everything

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-container">
-      <h1>Countdown Timer</h1>
+      <h1>Final Count</h1>
     </div>
   );
 }
